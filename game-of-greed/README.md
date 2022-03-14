1b788a00312f289c9bb320f7fd69bd9a25710613
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-03-14 22:25:54 +0300

    latest version

---
+++ game-of-greed/README.md
@@ -0,0 +1,9 @@
+## Title:
+Game of Greed
+
+## Description:
+This contains 2 classes one for the Game Logic that calculates the score of the dice rolls and tuple of the rolls
+and the 2nd is Game Banker class which checks if the score is stored in the bank from the shelf or clear the shelf if a zilch happens and disregrad the score colleccted
+
+## Link:
+[]()
--- game-of-greed/game_of_greed/game_banker.py
+++ game-of-greed/game_of_greed/game_banker.py
@@ -1,5 +1,18 @@
 # from game_of_greed.game_logic import ameLogic
 class Banker:
+
+    '''
+
+    Banker class that has 4 methods:
+
+    __init__: which instiate when class is called which takes in 2 optional arguments balance and balance.
+    shelf: which takes 1 argument an integer and calculate the total and returns an integer
+    bank: which takes no arguments and stores the total balance
+    clear_shelf: which takes no arguments and clears the shelf and assign it to 0
+
+
+    '''
+
     def __init__(self , balance = 0 , shelved = 0  ):
         # give the balance and shelved a defulte value
         self.balance = balance 

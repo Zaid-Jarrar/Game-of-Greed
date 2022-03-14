149e9147aeb7aed5904c0d53d223e2403c337ff9
Ayat <alkayedayat93@gmail.com>
2022-03-14 19:04:36 +0300

    banker

--- game-of-greed/game_of_greed/game_banker.py
+++ game-of-greed/game_of_greed/game_banker.py
@@ -1,17 +1,21 @@
 # from game_of_greed.game_logic import ameLogic
 class Banker:
-    def __init__(self ):
-        self.balance = 0 
-        self.shelved = 0
+    def __init__(self , balance = 0 , shelved = 0  ):
+        # give the balance and shelved a defulte value
+        self.balance = balance 
+        self.shelved = shelved
 
-    def shelf(self,num):
-        self.shelved+=num
-        return self.shelved
+    def shelf(self,score):
+        # shelved as a counter for the score
+        self.shelved += score
+        return 
 
     def bank(self):
+        # balance in bank is commulated from shelf
         self.balance+=self.shelved
         self.shelved=0
-        return self.balance
+        return 
 
+        # when zelsh mush be return to zero
     def clear_shelf(self):
         self.shelved=0

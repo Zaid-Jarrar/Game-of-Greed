eccd09bac07aa8e12f77b42922a4cdad29464f34
moayadabukhadra <moayadabukhadra54@gmail.com>
2022-03-22 16:12:46 +0300

    final_version_gog3

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -10,7 +10,7 @@ class Game:
         self.banker = Banker()
         self.logic = GameLogic()
         self.banker.bank
-
+        self.cheater=self.logic.validate_input
 
 
     def play(self):
@@ -30,8 +30,8 @@ class Game:
                 # shelved = banked.shelf(score)
                 # print(shelved)
                 # shelf = 0
-
-                if choice != 'r':
+                
+                if choice != 'r' :
                     
                     print(f"Starting round {round}")
 
@@ -48,6 +48,8 @@ class Game:
                 print(",".join(nums))
                
 
+               
+                
                 possible_score = logic.calculate_score(rolled_dice)# rolled_dice
                 
                 #Zilch
@@ -55,34 +57,18 @@ class Game:
                     print('Zilch!!! Round over')
                     print(f'You banked 0 points in round {round}')
                     print(f'Total score is {banked.balance} points')
+                    banked.clear_shelf()
                     round += 1
                     # print(nums)
-                    
+                    choice=0
                     # print(f"Starting round {round}")    
                     rem_dice = 6
-                    break
-                # if possible_score == 0:
-                #     print('Zilch!!! Round over')
-                #     print(f'You banked 0 points in round {round}')
-                #     print(f'Total score is {banked.balance} points')
-                #     round += 1
-                #     print(f"Starting round {round}")
+                    continue
 
-                #     print(f"Rolling {rem_dice} dice...")# dynamic dice counter step 1
-                #     rolled_dice = self.roller(rem_dice)# dynamic dice counter step 2
-                #     # rolled_dice = (2,6,4,3,3,6)
-                #     nums = []
-                #     for i in rolled_dice:
-                #         nums.append(str(i))
-                #     print(",".join(nums))              
-                    
-                    
-                #I DONT KNOW HOW IT WORKED 
-
-
-               
+                
+                
                 decision = input("Enter dice to keep (no spaces), or (q)uit: ")
-
+                
 
                 #BANKING HERE -----------
                 result = banked.balance
@@ -98,58 +84,84 @@ class Game:
                 if banked.balance != 0 and decision == 'q':
                         print(f'Total score is {result} points')
                         print(f'Thanks for playing. You earned {result} points') 
-                        break   
-                else:
+                        break 
+                  
+                new_list = []
+                selected_dice = tuple(decision)
+                for elem in selected_dice:
+                    # if elem in rolled_dice:
+                    
+                    new_list.append(int(elem))
+                    # else:
+                    #     print('cheater')
+                    #     #-------------------------------------------------------HERE
+                    #     break
+
+                
+                    
+                # print(new_list)
+                new_tuple = tuple(new_list)
+                # for single_dice in new_tuple:
+                #     if single_dice not in rolled_dice:
+                #         print('cheater')
+                #         break
+                # print(new_tuple)
+                self.cheater(rolled_dice,new_tuple)
+                cheater= self.cheater(rolled_dice,new_tuple)
+                
+                if cheater:
+                    print('Cheater!!! Or possibly made a typo...')
+                    nums = []
+                    for i in rolled_dice:
+                        nums.append(str(i))
+                    print(",".join(nums))
+                    banked.clear_shelf()
+                    decision = input("Enter dice to keep (no spaces), or (q)uit: ")
                     new_list = []
                     selected_dice = tuple(decision)
                     for elem in selected_dice:
-                        # if elem in rolled_dice:
+                       
                         
                         new_list.append(int(elem))
-                        # else:
-                        #     print('cheater')
-                        #     #-------------------------------------------------------HERE
-                        #     break
+                       
 
                     
                         
                     # print(new_list)
                     new_tuple = tuple(new_list)
-                    # for single_dice in new_tuple:
-                    #     if single_dice not in rolled_dice:
-                    #         print('cheater')
-                    #         break
-                    # print(new_tuple)
-                   
-                    score = GameLogic.calculate_score(new_tuple)
-                    shelved = banked.shelf(score)
+                    
+                score = GameLogic.calculate_score(new_tuple)
+                shelved = banked.shelf(score)
 
-                    # print(score)
-                   
-                    # remaining dice counter
-                    rem_dice = rem_dice - len(selected_dice)
+                # print(score)
+                
+                # remaining dice counter
+                rem_dice = rem_dice - len(selected_dice)
 
-                        
-                    print(
-    
-                        f"You have {shelved} unbanked points and {rem_dice} dice remaining"
-                    )
+                    
+                print(
+
+                    f"You have {shelved} unbanked points and {rem_dice} dice remaining"
+                )
+                if rem_dice == 0:
+                    rem_dice = 6
+                choice = input("(r)oll again, (b)ank your points or (q)uit ")
+
+                # choice = input("(r)oll again, (b)ank your points or (q)uit: ")
+
+                if choice == "b":                        
+                    print(f"You banked {shelved} points in round {round}")
+                    print(f"Total score is {banked.bank()[1]} points")
+                    round += 1
+                    rem_dice = 6
+                if choice=="q":
+                    print(f'Total score is {result} points')
+                    print(f'Thanks for playing. You earned {result} points')                  
+                    break
+                if choice == 'r':
                     if rem_dice == 0:
                         rem_dice = 6
-                    choice = input("(r)oll again, (b)ank your points or (q)uit ")
-
-                    # choice = input("(r)oll again, (b)ank your points or (q)uit: ")
-
-                    if choice == "b":                        
-                        print(f"You banked {shelved} points in round {round}")
-                        print(f"Total score is {banked.bank()[1]} points")
-                        round += 1
-                        rem_dice = 6
-
-                    elif choice == 'r':
-                        if rem_dice == 0:
-                            rem_dice = 6
-                        continue
+                    continue
                 
 
                 
